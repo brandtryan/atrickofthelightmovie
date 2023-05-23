c234296a2b0230ec05ad558a339ce32bcdd68e86
Brandt <emailme@brandtryan.com>
2023-05-23 07:41:32 -0400

    video square

--- sketch.js
+++ sketch.js
@@ -1,67 +1,37 @@
-var easycam,
-	state = {
-		distance: 208,
-		center: [5, 3, -13],
-		rotation: [0.3, -0.4, -0.1, 0.85],
-	},
-	f,
-	img,
-	x = 0,
-	y = 20;
+// video source: https://vimeo.com/90312869
+// let img;
+let vid;
+let theta = 0;
 
-function preload() {
-	img = loadImage('assets/images/stills/137.webp');
-	f = loadFont('assets/CourierPrime-Regular.ttf');
-}
-
-document.oncontextmenu = () => false;
 function setup() {
-	createCanvas(windowWidth, windowHeight, WEBGL);
-	//setAttributes('antialias', true);
-	easycam = createEasyCam();
+	createCanvas(1080, 1920, WEBGL);
 
-	// set initial camera state
-	easycam.setState(state, 1000); // animate to state in 1 second
-	easycam.state_reset = state; // state to use on reset
-
-	// use the loaded font
-	textFont(f);
-	textSize(16);
-	stroke(50, 50, 52);
-	strokeWeight(0.5);
+	// img = loadImage('assets/cat.jpg');
+	vid = createVideo(['assets/chaos.mp4']);
+	vid.elt.muted = false;
+	vid.loop();
+	vid.hide();
 }
 
 function draw() {
-	// 3D section
-	background(32);
-	// lights();
-	imageMode(CENTER);
-	image(img, 0, 0);
-
-	// 2D screen-aligned rendering section
-	easycam.beginHUD();
-	noLights();
-	let state = easycam.getState();
-
-	// Render the background box for the HUD
-	noStroke();
-	fill(0);
-	rect(x, y, 20, 100);
-	fill(50, 50, 52, 200); // a bit of transparency
-	rect(x + 20, y, 380, 100);
-
-	// Render the labels
-	fill(69, 161, 255);
-	text('Distance:', x + 35, y + 25);
-	text('Center:  ', x + 35, y + 25 + 20);
-	text('Rotation:', x + 35, y + 25 + 40);
-	text('Framerate:', x + 35, y + 25 + 60);
-
-	// Render the state numbers
-	fill(69, 161, 255);
-	text(nfs(state.distance, 1, 2), x + 125, y + 25);
-	text(nfs(state.center, 1, 2), x + 125, y + 25 + 20);
-	text(nfs(state.rotation, 1, 3), x + 125, y + 25 + 40);
-	text(nfs(frameRate(), 1, 2), x + 125, y + 25 + 60);
-	easycam.endHUD();
+	background(0);
+	translate(-0, -0, -0);
+	push();
+	rotateZ(theta * mouseX * 0.001);
+	rotateX(theta * mouseX * 0.001);
+	rotateY(theta * mouseX * 0.001);
+	//pass image as texture
+	// texture(vid);
+	// sphere(250);
+	// pop();
+	translate(-0, -0, -0);
+	push();
+	rotateZ(theta * 0.1);
+	rotateX(theta * 0.1);
+	rotateY(theta * 0.1);
+	normalMaterial();
+	texture(vid);
+	box(300, 300, 300);
+	pop();
+	theta += 0.02;
 }
